7462b8a47625f4ae0323cde153683843f8f42d08
wall-of-pain <105595965+wall-of-pain@users.noreply.github.com>
2022-05-17 12:25:55 +0530

    Update open.ps1

--- open.ps1
+++ open.ps1
@@ -1,3 +1,2 @@
-ECHO ready?!
 Start-Sleep -Seconds 5
-for ($i=1; $i -le 100000; $i++) {Start https://google.com}
+for ($i=1; $i -le 2; $i++) {Start https://google.com;Start https://lms.bennett.edu.in;Start https://bennett.collpoll.com;Start https://www.bennett.edu.in;Start https://www.bing.com;Start https://yandex.com;Start https://www.instagram.com}
