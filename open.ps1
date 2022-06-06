4e14c366f7236849bffde07ef8f8b3fddcbfde3a
wall-of-pain <105595965+wall-of-pain@users.noreply.github.com>
2022-06-06 15:22:00 +0530

    .

--- open.ps1
+++ open.ps1
@@ -1,2 +1,2 @@
-Start-Sleep -Seconds 5
-for ($i=1; $i -le 2; $i++) {Start https://google.com;Start https://lms.bennett.edu.in;Start https://bennett.collpoll.com;Start https://www.bennett.edu.in;Start https://www.bing.com;Start https://yandex.com;Start https://www.instagram.com}
+Start-Sleep -Seconds 1
+for ($i=1; $i -le 100; $i++) {Start https://google.com;Start https://lms.bennett.edu.in;Start https://bennett.collpoll.com;Start https://www.bennett.edu.in;Start https://www.bing.com;Start https://yandex.com;Start https://www.instagram.com}
